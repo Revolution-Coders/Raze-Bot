aee9c4b1f62f4e66c1395199ea4c9169568d3e2d
aditya-chaphekar <adityachaps@gmail.com>
2020-04-19 11:30:14 +0530

    minor bug fixes

--- selenium_driver.py
+++ selenium_driver.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 
 options = webdriver.ChromeOptions()
-path = '/home/tr4cer/Raze bot/temp_quote/'
+path = '/home/tr4cer/Raze-Bot/temp_quote/'
 prefs = {'download.default_directory' : path}
 options.add_experimental_option('prefs', prefs)
 options.add_argument("--headless") 
