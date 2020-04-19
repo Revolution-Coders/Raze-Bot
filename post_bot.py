eff8e83638d0049d70ac676c1ded6a1bc06936cd
aditya-chaphekar <adityachaps@gmail.com>
2020-04-19 11:25:47 +0530

    minor bug fixes

--- post_bot.py
+++ post_bot.py
@@ -9,4 +9,5 @@ def post():
 	bot = Bot()   
 	bot.login(username = usr,password = psw) 
 	bot.upload_photo(path+fileName+'.jpg', caption ="Test") 
-	os.remove(path+fileName+'.jpg.REMOVE_ME')
+	#os.remove(path+fileName+'.jpg.REMOVE_ME')
+	os.rename(path+fileName+'.jpg.REMOVE_ME',path+fileName+'.jpg')
